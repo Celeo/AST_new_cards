1c2c87345a265e576738d2eb07a1bc58aec7d4b3
Celeo <celeodor@gmail.com>
2019-05-31 18:54:33 -0700

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
 import './index.css'
 
@@ -72,88 +73,166 @@ const getRandomCard = () => {
   return CARDS[Math.floor(Math.random() * Math.floor(6))]
 }
 
-const Controls = () => {
-  const [main, setMain] = useState(EMPTY_CARD)
-  const [seals, setSeals] = useState([EMPTY, EMPTY, EMPTY])
-
-  const draw = () => {
-    setMain(getRandomCard())
-  }
-  const redraw = () => {
-    let nextCard = main
-    while (nextCard === main) {
-      nextCard = getRandomCard()
+class SkillButton extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      cooldown: 0,
+      timerRef: null,
+      charges: props.maxCharges || 0
     }
-    setMain(nextCard)
-  }
-  const play = () => {
-    const nextSeals = seals
-    nextSeals.push(main.seal)
-    if (nextSeals.length > 3) {
-      nextSeals.shift()
-    }
-    setSeals(nextSeals)
-    setMain(EMPTY_CARD)
-  }
-  const divination = () => {
-    setSeals([EMPTY, EMPTY, EMPTY])
-  }
-  const divinationEnabled = () => {
-    let count = 0
-    for (let seal of seals) {
-      if (seal !== EMPTY) {
-        count++
-      }
-    }
-    return count > 1
-  }
-  const divinationStrength = () => {
-    let nonEmptySeals = seals.filter(s => s !== EMPTY)
-    if (nonEmptySeals.length !== 3) {
-      return 0
-    }
-    const uniqueSize = new Set(nonEmptySeals).size
-    if (uniqueSize === 1 || uniqueSize === 2) {
-      return 3
-    }
-    if (uniqueSize === 3) {
-      return 6
-    }
-    return 0
+    this.onClick = this.onClick.bind(this)
   }
 
+  onClick () {
+    if (this.props.cooldown) {
+      this.setState({ cooldown: this.props.cooldown })
+      const timerRef = setInterval(() => {
+        if (this.state.cooldown === 0) {
+          clearInterval(this.state.timerRef)
+          return
+        }
+        this.setState({ cooldown: this.state.cooldown - 1 })
+      }, 1000)
+      this.setState({ timerRef })
+    }
+    this.props.onClick()
+  }
+
+  render () {
+    return (
+      <button
+        onClick={ this.onClick }
+        disabled={ this.props.disabled() || this.state.cooldown > 0 }
+      >
+        { this.props.text }{ this.state.cooldown > 0 ? ' (' + this.state.cooldown + 's)' : null }
+      </button>
+    )
+  }
+}
+
+SkillButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+  cooldown: PropTypes.number.isRequired,
+  maxCharges: PropTypes.number
+}
+
+const CardStatus = (props) => {
   return (
     <div>
       <div className="clearfix">
-        <p className="float-left">Main:</p>
+        <p className="float-left">Held:</p>
         {
-          main !== EMPTY_CARD
+          props.held !== EMPTY_CARD
             ? (
               <div className="float-right">
-                <img src={main.image} />
-                <span> <strong>The { main.name }</strong>
-                  { ': ' + (main.forMelee ? '6% dmg up for melee, 3% for ranged' : '6% dmg up for ranged, 3% for melee') + '; ' + main.seal + ' seal' }
+                <img src={props.held.image} />
+                <span> <strong>The { props.held.name }</strong>
+                  { ': ' + (props.held.forMelee ? '6% dmg up for melee, 3% for ranged' : '6% dmg up for ranged, 3% for melee') + '; ' + props.held.seal + ' seal' }
                 </span>
               </div>
             )
             : null
         }
       </div>
-      <p>Seals: <span>{ seals.join(', ') }</span></p>
-      <br />
-      <button onClick={draw} disabled={main !== EMPTY_CARD}>Draw</button>
-      <button onClick={redraw} disabled={main === EMPTY_CARD}>Redraw</button>
-      <button onClick={play} disabled={main === EMPTY_CARD}>Play</button>
-      <button onClick={divination} disabled={!divinationEnabled()}>Divination</button>
-      <br />
-      <p>Divination strength: { divinationStrength() + '%' }</p>
-      <br />
-      <p>Note: <strong>Redraw</strong> has 3 charges on a 30-second charge timer.</p>
+      <p>Seals: <span>{ props.seals.join(', ') }</span></p>
     </div>
   )
 }
 
-// TODO Minor Arcana
+CardStatus.propTypes = {
+  held: PropTypes.object.isRequired,
+  seals: PropTypes.array.isRequired
+}
+
+class Controls extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      held: EMPTY_CARD,
+      seals: [EMPTY, EMPTY, EMPTY]
+    }
+    this.draw = this.draw.bind(this)
+    this.redraw = this.redraw.bind(this)
+    this.play = this.play.bind(this)
+    this.divination = this.divination.bind(this)
+    this.divinationEnabled = this.divinationEnabled.bind(this)
+    this.divinationStrength = this.divinationStrength.bind(this)
+  }
+
+  draw () {
+    this.setState({ held: getRandomCard() })
+  }
+
+  redraw () {
+    let nextCard = this.state.held
+    while (nextCard === this.state.held) {
+      nextCard = getRandomCard()
+    }
+    this.setState({ held: nextCard })
+  }
+
+  play () {
+    const nextSeals = this.state.seals
+    nextSeals.push(this.state.held.seal)
+    if (nextSeals.length > 3) {
+      nextSeals.shift()
+    }
+    this.setState({
+      seals: nextSeals,
+      held: EMPTY_CARD
+    })
+  }
+
+  divination () {
+    this.setState({ seals: [EMPTY, EMPTY, EMPTY] })
+  }
+
+  divinationEnabled () {
+    let count = 0
+    for (let seal of this.state.seals) {
+      if (seal !== EMPTY) {
+        count++
+      }
+    }
+    return count > 1
+  }
+
+  divinationStrength () {
+    let nonEmptySeals = this.state.seals.filter(s => s !== EMPTY)
+    if (nonEmptySeals.length !== 3) {
+      return 0
+    }
+    const uniqueSize = new Set(nonEmptySeals).size
+    if (uniqueSize <= 2) {
+      return 3
+    }
+    return 6
+  }
+
+  render () {
+    return (
+      <div>
+        <CardStatus held={this.state.held} seals={this.state.seals} />
+        <br />
+        <div className="button-row">
+          <SkillButton className="skill-button" onClick={this.draw} disabled={() => this.state.held !== EMPTY_CARD} text="Draw" cooldown={30} />
+          <SkillButton className="skill-button" onClick={this.redraw} disabled={() => this.state.held === EMPTY_CARD} text="Redraw" cooldown={30} maxCharges={3} />
+          <SkillButton className="skill-button" onClick={this.play} disabled={() => this.state.held === EMPTY_CARD} text="Play" cooldown={0} />
+          <SkillButton className="skill-button" onClick={() => {}} disabled={() => true} text="Sleeve Draw" cooldown={180} />
+          <SkillButton className="skill-button" onClick={() => {}} disabled={() => true} text="Minor Arcana" cooldown={1} />
+          <SkillButton className="skill-button" onClick={this.divination} disabled={() => !this.divinationEnabled()} text="Divination" cooldown={180} />
+        </div>
+        <br />
+        <p>Divination strength: { this.divinationStrength() + '%' }</p>
+      </div>
+    )
+  }
+}
+
+Controls.propTypes = {}
 
 const Index = () => {
   return (
